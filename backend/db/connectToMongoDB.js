70c47f5088175f431c8970ceb808c9fa5490081d
kaleem-Durrani <kalimdurrani22@gmail.com>
2024-04-22 00:10:46 -0700

    initial commit

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("connected to mongo db");
+  } catch (error) {
+    console.log("Error connecting to mongo db", error.message);
+  }
+};
+
+export default connectToMongoDB;
